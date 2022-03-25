333150669a7c01ec15e75fc450a63cb8798ff36a
Andre Davcev <>
2022-03-25 01:26:36 -0400

    npx nx generate @nxtend/ionic-angular:application --name=xrpl-wallet-demo --template=list --no-interactive

---
+++ packages/xrpl-wallet-demo/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'xrpl-wallet-demo',
+  webDir: '../../dist/packages/xrpl-wallet-demo',
+  bundledWebRuntime: false,
+};
+
+export default config;
